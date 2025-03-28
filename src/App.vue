6740fd80dd6a881efbe6a1a08d6cdd53580fc1ac
Firdavs-22 <firdavsgaybullayev22@gmail.com>
2025-03-28 16:27:48 +0900

    code base

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app :theme="appStore.theme">
+    <router-view/>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import {useThemeStore} from "@/stores/theme";
+
+const appStore = useThemeStore()
+</script>
